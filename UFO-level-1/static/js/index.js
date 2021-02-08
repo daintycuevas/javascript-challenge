b531a95510ef7a66441b48cfe7779ab173e2ff8f
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-08 00:12:58 -0500

    add image folder

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -14,5 +14,28 @@ data.forEach(function(ufoReport) {
   });
 });
 
+var button = d3.select("#button");
 
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#patient-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(people);
+
+  var filteredData = people.filter(person => person.bloodType === inputValue);
+
+  console.log(filteredData);
 
