94dd01ee80a264c20c10178551ff7b20c2a3075c
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-07 21:53:25 -0500

    update

---
+++ UFO-level-1/static/js/index.js
@@ -0,0 +1,13 @@
+var data = data;
+
+function response() {
+  return data;
+}
+console.log(response);
+
+// data.forEach(function(ufoReport) {
+//   console.log(ufoReport);
+// });
+
+// ufodata.append('imagefile', file);
+
